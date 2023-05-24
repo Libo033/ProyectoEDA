70e0afc3d4a2362cc903ec13e3ec3261a3f8b64e
Libo033 <113865453+Libo033@users.noreply.github.com>
2023-05-24 20:12:31 -0300

    Cambios de Funciones y main mejorado

--- ProyectoEDA.c
+++ ProyectoEDA.c
@@ -6,15 +6,14 @@
 
 int ObtenerRandomNum(int a, int b);
 int seEncuentraEn(char letra, char *palabra);
+int compararPalabras(char *random, char *usuario);
 
 void imprimirVerde(char a);
 void imprimirAmarillo(char a);
 void toUpper(char *a);
-void wordle();
-void compararPalabras(char *random, char *usuario);
 
 char *ingresarPalabra();
-char *palabraElegida();
+char *palabraRandom();
 
 int main(void)
 {
@@ -22,12 +21,14 @@ int main(void)
   // Funcion srand: https://www.tutorialspoint.com/c_standard_library/c_function_srand.htm
   srand(time(NULL));
 
-  short menu = 1;
+  // Variables y punteros WORDLE
+  short menu = 1, intentos = 0, finalJuego = 0;
+  char *result = NULL, *palabraIngresada = NULL;
 
   do
   {
     system("cls");
-    printf("Bienvenido\n0-Salir.\n1-Jugar al WORDLE.\nIngresar: ");
+    printf("Bienvenido\n0-Salir.\n1-Jugar al WORDLE.\n2-Jugar al AHORCADO\n3-Ver puntajes\nIngresar: ");
     scanf("%hd", &menu);
 
     switch (menu)
@@ -35,11 +36,50 @@ int main(void)
     case 1:
       system("cls");
 
-      wordle();
+      intentos = 0, finalJuego = 0;
+      result = palabraRandom();
+
+      printf("WORDLE\nTienes 5 intentos para descubrir la palabra de 5 letras.\nIngresar palabra\n");
+
+      while (intentos < 5 && finalJuego == 0)
+      {
+        palabraIngresada = ingresarPalabra();
+        printf("            ");
+        finalJuego = compararPalabras(result, palabraIngresada);
+        printf("\n");
+
+        intentos++;
+        free(palabraIngresada);
+      }
+
+      if (finalJuego == 1)
+      {
+        printf("\nFelicidades Ganaste!!!\n");
+      }
+      else
+      {
+        printf("Perdiste :(\nLa palabra era %s", result);
+      }
+
+      free(result);
+
+      printf("\n");
+      system("pause");
+      break;
+    case 2:
+      system("cls");
+
+      printf("AHORCADO\n");
 
       system("pause");
       break;
+    case 3:
+      system("cls");
 
+      printf("Puntaje\n");
+
+      system("pause");
+      break;
     default:
       break;
     }
@@ -58,115 +98,20 @@ int ObtenerRandomNum(int a, int b)
   return rand() % (b - a + 1) + a;
 }
 
-void imprimirVerde(char a) // https://elpuig.xeill.net/Members/vcarceler/articulos/escape-ansi
-{ // Transformar en return para poder almacenar la palabra entera y que cada letra tenga sus respectivos colores.
-  char *verde = "\033[0;40;1;32m";
-  char *normal = "\033[0m";
-
-  printf("%s%c%s", verde, a, normal);
-}
-
-void imprimirAmarillo(char a) // https://elpuig.xeill.net/Members/vcarceler/articulos/escape-ansi
-{ // Transformar en return para poder almacenar la palabra entera y que cada letra tenga sus respectivos colores.
-  char *amarillo = "\033[0;40;1;33m";
-  char *normal = "\033[0m";
-
-  printf("%s%c%s", amarillo, a, normal);
-}
-
-char *palabraElegida()
-{
-  // Se crean el puntero result que es el que retorna el valor final.
-  // y el puntero FILE que abrira el archivo palabras.txt
-  char *result = NULL;
-  FILE *archivo = NULL;
-  archivo = fopen("..\\palabras.txt", "rt");
-
-  if (archivo == NULL) // Controla el error
-  {
-    printf("\nError: No se pudo leer el archivo");
-    return result;
-  }
-
-  result = (char *)malloc(5); // Se establece el tamaño del string que se devuelve
-  if (result == NULL)         // Controla el error
-  {
-    printf("\nError: No se puedo asignar memoria");
-    return result;
-  }
-
-  // Se recibe el num. random para encontrar la palabra random
-  int posPalabra = ObtenerRandomNum(1, 30);
-
-  // Se establece la posicion al inicio de la palabra con el num random
-  // y se lee 5 caracteres
-  fseek(archivo, posPalabra * 7 - 7, SEEK_SET);
-  fread(result, 1, 5, archivo);
-  result[5] = '\0'; // Final
-  fclose(archivo);  // Se cierra el archivo.
-
-  toUpper(result);
-
-  return result;
-}
-
-char *ingresarPalabra()
-{
-  char *result = NULL;
-  result = (char *)malloc(5); // Se establece el tamaño del string que se devuelve
-
-  if (result == NULL)
-  {
-    printf("\nError: No se puedo asignar memoria");
-    return result;
-  }
-
-  scanf("%5s", result);
-
-  toUpper(result);
-
-  return result;
-}
-
-void toUpper(char *a)
-{
-  int len = strlen(a);
-
-  for (short i = 0; i < len; i++)
-  {
-    a[i] = toupper(a[i]);
-  }
-}
-
-void wordle() // Crear en el main
-{
-  // short intentos = 0;
-  char *result = NULL, *palabraIngresada = NULL;
-  result = palabraElegida();
-  printf("%s\n", result);
-
-  printf("WORDLE\nTienes 5 intentos para descubrir la palabra de 5 letras.\nIngresar palabra\n");
-  palabraIngresada = ingresarPalabra();
-  compararPalabras(result, palabraIngresada);
-
-  free(result);
-}
-
-void compararPalabras(char *random, char *usuario)  // Que devuelva la palabra con cada color.
+int compararPalabras(char *random, char *usuario) // Que devuelva la palabra con cada color.
 {
   short i;
 
-  if (strcmp(random, usuario) == 0)
+  if (strcmp(random, usuario) == 0) // CORRECTO
   {
     for (i = 0; i < (short)strlen(usuario); i++)
-    {
       imprimirVerde(usuario[i]);
-    }
-    printf("\nCorrecto!\n");
+
+    return 1;
   }
   else
   {
-    for (i = 0; i < (short)strlen(usuario); i++)
+    for (i = 0; i < (short)strlen(usuario); i++) // INCORRECTO - COMPARA POR LETRAS
     {
       if (usuario[i] == random[i])
       {
@@ -181,9 +126,8 @@ void compararPalabras(char *random, char *usuario)  // Que devuelva la palabra c
         printf("%c", usuario[i]);
       }
     }
+    return 0;
   }
-
-  printf("\n");
 }
 
 int seEncuentraEn(char letra, char *palabra)
@@ -191,10 +135,84 @@ int seEncuentraEn(char letra, char *palabra)
   for (short i = 0; i < (short)strlen(palabra); i++)
   {
     if (palabra[i] == letra)
-    {
       return 0;
-    }
   }
 
   return 1;
+}
+
+void imprimirVerde(char a) // https://elpuig.xeill.net/Members/vcarceler/articulos/escape-ansi
+{
+  char *verde = "\033[0;40;1;32m";
+  char *normal = "\033[0m";
+
+  printf("%s%c%s", verde, a, normal);
+}
+
+void imprimirAmarillo(char a)
+{
+  char *amarillo = "\033[0;40;1;33m";
+  char *normal = "\033[0m";
+
+  printf("%s%c%s", amarillo, a, normal);
+}
+
+void toUpper(char *a)
+{
+  int len = strlen(a);
+
+  for (short i = 0; i < len; i++)
+    a[i] = toupper(a[i]);
+}
+
+char *palabraRandom()
+{
+  // Se crean el puntero result que es el que retorna el valor final.
+  // y el puntero FILE que abrira el archivo palabras.txt
+  char *result = NULL;
+  FILE *archivo = NULL;
+  archivo = fopen("..\\palabras.txt", "rt");
+
+  if (archivo == NULL) // Controla el error
+  {
+    printf("\nError: No se pudo leer el archivo");
+    return result;
+  }
+
+  result = (char *)malloc(5 * sizeof(char)); // Se establece el tamaño del string que se devuelve
+  if (result == NULL)         // Controla el error
+  {
+    printf("\nError: No se puedo asignar memoria");
+    return result;
+  }
+
+  int posPalabra = ObtenerRandomNum(1, 30);
+
+  // Se establece la posicion al inicio de la palabra con el num random y se lee 5 caracteres
+  fseek(archivo, posPalabra * 7 - 7, SEEK_SET);
+  fread(result, 1, 5, archivo);
+  result[5] = '\0'; // Final
+  fclose(archivo);  // Se cierra el archivo.
+
+  toUpper(result);
+
+  return result;
+}
+
+char *ingresarPalabra()
+{
+  char *result = NULL;
+  result = (char *)malloc(5 * sizeof(char)); // Se establece el tamaño del string que se devuelve
+
+  if (result == NULL)
+  {
+    printf("\nError: No se puedo asignar memoria");
+    return result;
+  }
+
+  scanf("%5s", result);
+
+  toUpper(result);
+
+  return result;
 }
